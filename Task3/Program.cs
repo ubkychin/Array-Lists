9fc9a592d336315ee0957fb157d10f67c3c44865
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-17 19:27:18 +1000

    Task 2.1

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        int a=0, b=0, c=0, d=0, e=0;
+
+        List<int> num = new List<int>();
+        System.Console.WriteLine("Gimme 5 numbers!! :");
+        Console.ReadLine();
+        num.Contains(a);
+        Console.ReadLine();
+        num.Contains(b);
+        Console.ReadLine();
+        num.Contains(c);
+        Console.ReadLine();
+        num.Contains(d);
+        Console.ReadLine();
+        num.Contains(e);
+        Console.ReadLine();
+
+
+        System.Console.WriteLine(a);
+
         }
     }
 }
