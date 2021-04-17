6b1c099c427b38ea932f6ae1393425ad491a7de8
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-17 18:13:09 +1000

    Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Linq;
 namespace Task2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        int[] number = new int[10];
+        number[0] = 34;
+        number[1] = 5;
+        number[2] = 67;
+        number[3] = 1;
+        number[4] = 99;
+        number[5] = 34;
+        number[6] = 44;
+        number[7] = 78;
+        number[8] = 34;
+        number[9] = 0;     
+    
+
+      Console.WriteLine("Total is: " + number.Sum());
+        
         }
     }
 }
