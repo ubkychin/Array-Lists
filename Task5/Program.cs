b39754a651cbd800be916291c49ec6249e7b681b
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-22 17:02:13 +1000

    Fifth Task

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Collections;
 namespace Task5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        string input;
+
+        System.Console.WriteLine("Guess da word :");
+        input = Console.ReadLine();
+        List<string> Word = new List<string>();
+
+        while (input != "Stop") {  
+        Word.Add(input);
+        input = Console.ReadLine();
+        } 
+        Console.WriteLine("Good Job");
+        
+        Word.Reverse();
+
+        Console.WriteLine();
+        foreach(string List in Word)
+        {
+            Console.WriteLine(List);
+        }
         }
     }
 }
